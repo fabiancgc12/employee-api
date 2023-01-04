fdb2080dc6f8d6e7da9590d92a1f5617fe16d419
fabian graterol <fabiancgraterol@gmail.com>
2023-01-04 19:54:35 -0400

    throwing the error so the controller layer handles it

--- src/Employee/EmployeeService.ts
+++ src/Employee/EmployeeService.ts
@@ -3,7 +3,6 @@ import {EmployeeModel} from "./model/EmployeeModel.js";
 import {pgClient} from "../common/database/pgClient.js";
 import {PostgresErrorCodes} from "../common/database/PostgresErrorCodes.js";
 import {UniqueConstraintException} from "../common/exceptions/UniqueConstraintException.js";
-import {ServerException} from "../common/exceptions/ServerException.js";
 import {DatabaseError} from "pg";
 import {ResourceNotFoundException} from "../common/exceptions/ResourceNotFoundException.js";
 
@@ -21,7 +20,7 @@ export class EmployeeService {
             if (e instanceof DatabaseError && e.code == PostgresErrorCodes.DuplicatePrimaryKey){
                 throw new UniqueConstraintException("email")
             }
-            throw new ServerException()
+            throw e
         }
     }
 
