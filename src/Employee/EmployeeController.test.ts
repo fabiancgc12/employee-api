cc012957f664c05d08711a2a3de61ff0000121a9
fabian graterol <fabiancgraterol@gmail.com>
2023-01-05 21:26:44 -0400

    testing post employee controller

--- src/Employee/EmployeeController.test.ts
+++ src/Employee/EmployeeController.test.ts
@@ -1,27 +1,79 @@
-import {EmployeeController} from "./EmployeeController.js";
 import request from "supertest";
 import app from "../app.js";
 import {mockCreateEmployeeDto} from "../common/utils/mockCreateEmployeeDto.js";
+import {pgClient} from "../common/database/pgClient.js";
+
 describe("Employee controller",() => {
 
-    it('should create one employee without boss', async function () {
-        const dto = mockCreateEmployeeDto()
-        return request(app)
-            .post("users")
-            .send(dto)
-            .expect(201)
-            .expect('Content-Type', /json/)
-            .then(res => {
-                expect(res).toMatchObject({
-                    id:expect.any(String),
-                    firstName:dto.firstName,
-                    lastName:dto.lastName,
-                    email:dto.email,
-                    boss:undefined,
-                    dateOfBirth:dto.dateOfBirth,
-                    createdAt:expect.any(Date),
-                    updatedAt:expect.any(Date)
+    beforeAll(() => {
+
+    })
+
+    afterAll(async () => {
+        await pgClient.end()
+    })
+
+    describe('POST/USERS', function () {
+
+        it('should create one employee without boss', async function () {
+            const dto = mockCreateEmployeeDto()
+            return request(app)
+                .post("/users")
+                .send(dto)
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .then(res => {
+                    expect(res.body).toMatchObject({
+                        id:expect.any(String),
+                        firstName:dto.firstName,
+                        lastName:dto.lastName,
+                        email:dto.email,
+                        dateOfBirth:dto.dateOfBirth.toISOString(),
+                        createdAt:expect.any(String),
+                        updatedAt:expect.any(String)
+                    })
+                    expect(res.body).not.toHaveProperty("boss")
                 })
-            })
+        });
+
+        it('should create one employee with a boss', async function () {
+            let dto = mockCreateEmployeeDto();
+            const {body: boss} = await request(app).post("/users").send(dto)
+            dto = mockCreateEmployeeDto({
+                boss:boss.id
+            });
+            return request(app)
+                .post("/users")
+                .send(dto)
+                .expect(201)
+                .expect('Content-Type', /json/)
+                .then(res => {
+                    expect(res.body).toMatchObject({
+                        id:expect.any(String),
+                        firstName:dto.firstName,
+                        lastName:dto.lastName,
+                        email:dto.email,
+                        boss:dto.boss,
+                        dateOfBirth:dto.dateOfBirth.toISOString(),
+                        createdAt:expect.any(String),
+                        updatedAt:expect.any(String)
+                    })
+                })
+        });
+
+        it('should throw error when creating employee with repeated email', async function () {
+            const firstEmployeeDto = mockCreateEmployeeDto();
+            await request(app).post("/users").send(firstEmployeeDto)
+            const secondEmployeeDto = mockCreateEmployeeDto({
+                email:firstEmployeeDto.email
+            });
+            return request(app)
+                .post("/users")
+                .send(secondEmployeeDto)
+                .expect(409)
+        });
+
     });
+
+
 })
--- src/Employee/EmployeeController.ts
+++ src/Employee/EmployeeController.ts
@@ -1,13 +1,17 @@
 import {RequestHandler, Router} from 'express';
 import {BaseController} from "../common/controller/BaseController.js";
+import {EmployeeService} from "./EmployeeService.js";
 
 export class EmployeeController extends BaseController{
   readonly baseRoute = "/users";
   readonly router: Router;
 
+  private employeeService:EmployeeService
+
   constructor() {
     super()
-    this.router = Router()
+    this.router = Router();
+    this.employeeService = new EmployeeService()
     this.initializeRoutes()
   }
 
@@ -20,8 +24,13 @@ export class EmployeeController extends BaseController{
     res.send('respond with a resource');
   }
 
-  create:RequestHandler = async (req,res) => {
-    console.log(req.body)
-    res.send('holis');
+  create:RequestHandler = async (req,res,next) => {
+    try {
+      const dto = req.body
+      const employee = await this.employeeService.createOne(dto);
+      res.status(201).json(employee);
+    }catch (e) {
+      next(e)
+    }
   }
 }
